a06d3f76904d339edf3233463867779dcbf4acb5
Jake Lopez <jlope590@fiu.edu>
2017-04-12 01:43:24 -0400

    Add files via upload

---
+++ hw3.fs
@@ -0,0 +1,72 @@
+﻿type TERMINAL = IF|THEN|ELSE|BEGIN|END|PRINT|SEMICOLON|ID|EOF|ERROR
+type PARSETREE =
+| Lf of TERMINAL
+| IFBr of PARSETREE*PARSETREE*PARSETREE*PARSETREE*PARSETREE*PARSETREE
+| BEGBr of PARSETREE*PARSETREE*PARSETREE
+| PrintBr of PARSETREE*PARSETREE
+| SemiBR of PARSETREE*PARSETREE*PARSETREE
+
+let E = function
+    | [] -> failwith "ERROR @ E"
+    | x::xs -> match x with
+                | ID -> (Lf(ID), xs)
+                | _ -> failwith (sprintf "E: expected ID got %A" x)
+
+let eat token xs =
+    match xs with
+    | [] -> failwith "No tokens to eat"
+    | x::xs -> if x = token then xs else failwith (sprintf "EAT: expected %A got %A" token x)
+
+let rec S = function
+    |  [] -> failwith "Permature termination of input"
+    |  x::xs -> 
+            match x with 
+            | IF ->
+                let (T_E, remain) = xs |> E
+                let remain = remain |> eat THEN
+                let (T_S, remain) = remain |> S
+                let remain = remain |> eat ELSE
+                let (T_S2, remain) = remain |> S
+                (IFBr(Lf(IF), T_E, Lf(THEN), T_S, Lf(ELSE), T_S2), remain)
+            | BEGIN -> 
+                let (T_S, remain) = xs |> S
+                let (T_L, remain) = remain |> L
+                (BEGBr(Lf(BEGIN), T_S, T_L), remain)
+            | PRINT ->
+                let (T_E, remain) = xs |> E
+                (PrintBr(Lf(PRINT), T_E), remain)
+            | _ -> failwith (sprintf "S: Expected IF, BEGIN, PRINT got %A" x)
+and L = function
+    | [] -> failwith "Premature termination of input"
+    | x::xs -> 
+            match x with
+            | END -> (Lf(END), xs)
+            | SEMICOLON -> 
+               let (S_tree, remain) = xs |> S
+               let (L_tree, remain) = remain |> L
+               (SemiBR(Lf(SEMICOLON), S_tree, L_tree), remain)
+            | _ -> failwith (sprintf "L: Expected END, SEMICOLON got %A" x)
+
+let buildTree = function
+| [] -> Lf(ERROR)
+| xs -> let (S_tree, tokens) = xs |> S
+        if tokens <> [EOF] then failwith (sprintf "Want [EOF], got %A" tokens)
+        else S_tree
+
+let rec interpret_tree tree = 
+    match tree with
+    | IFBr (iftok, idtok, thentok, ex1, elsetok, ex2) -> 
+                                                        let exp1 = interpret_tree ex1
+                                                        let exp2 = interpret_tree ex2
+                                                        [IF; ID; THEN] @ exp1 @ [ELSE] @ exp2
+    | Lf (x) -> [x]
+    | PrintBr (x, y) -> let expy = interpret_tree y
+                        [PRINT] @ expy
+    | SemiBR (x, y, z) -> let expy = interpret_tree y
+                          let expz = interpret_tree z
+                          [SEMICOLON] @ expy @ expz
+    | BEGBr(x,y,z) -> let expy = interpret_tree y
+                      let expz = interpret_tree z
+                      [BEGIN] @ expy @ expz
+
+let buildandinterp toks = toks |> buildTree |> interpret_tree
---
+++ interp.fsx
@@ -0,0 +1,62 @@
+﻿// Skeleton file for PCF interpreter
+
+// This sets F# to read from whatever directory contains this source file.
+System.Environment.set_CurrentDirectory __SOURCE_DIRECTORY__;;
+
+#load "parser.fsx"
+
+// This lets us refer to "Parser.Parse.parsefile" simply as "parsefile",
+// and to constructors like "Parser.Parse.APP" simply as "APP".
+open Parser.Parse
+
+// Here I show you a little bit of the implementation of interp. Note how ERRORs
+// are propagated, how rule (6) is implemented, and how stuck evaluations
+// are reported using F#'s sprintf function to create good error messages.
+
+let rec subst e x t =
+    match e with
+    | IF(x1, y1, z1) -> IF((subst x1 x t), (subst y1 x t), (subst z1 x t))
+    | FUN(param, body) -> FUN(param, if param=x then body else subst body x t)
+    | ID(i) -> if i = x then t else ID(i)
+    | APP (e1, e2) -> APP (subst e1 x t, subst e2 x t)
+    | REC (param, body) -> REC(param, if param=x then body else subst body x t)
+    | _ -> e
+
+let rec interp = function
+| APP (e1, e2) ->
+    match (interp e1, interp e2) with
+    | (ERROR s, _)  -> ERROR s        // ERRORs are propagated
+    | (_, ERROR s)  -> ERROR s
+    | (SUCC, NUM n) -> NUM (n+1)      // Rule (6)
+    | (SUCC, v)     -> ERROR (sprintf "'succ' needs int argument, not '%A'" v)
+    | (PRED, NUM n) -> if n = 0 then NUM (0) else NUM (n-1)
+    | (PRED, v) -> ERROR (sprintf "'pred' needs int argument, not '%A'" v)
+    | (ISZERO, NUM n) -> if n = 0 then BOOL (true) else BOOL (false)
+    | (ISZERO, v) -> ERROR (sprintf "'iszero' needs int argument, not '%A'" v)
+    | (FUN (x,body), v) -> subst body x v |> interp
+    | (ID a, ID b) -> APP(ID a, ID b)
+    | (a, b) -> ERROR (sprintf "No case for '%A' or '%A'" a b)
+| NUM (n) -> NUM (n)
+| BOOL (b) -> BOOL (b)
+| SUCC -> SUCC
+| IF (guard, thenb, elseb) -> match (guard, thenb, elseb) with
+                              | (BOOL (b), e1, e2) -> let e1tok = interp e1
+                                                      let e2tok = interp e2
+                                                      if b = true then e1 else e2 
+                              | (APP(x, y), e1, e2) -> let eval = interp (APP (x,y))
+                                                       match eval with
+                                                        | BOOL (b) -> if b = true then interp e1 else interp e2
+                                                        | v -> ERROR (sprintf "'IF' guard requires a bool not a '%A'" v)
+
+                              | (v, _, _) -> ERROR (sprintf "'IF' guard requires a bool not a '%A'" v)
+| PRED -> PRED
+| ISZERO -> ISZERO
+| ID(s) -> ID(s)
+| REC(param, body) -> subst body param (REC(param, body)) |> interp
+| FUN(param, body) -> FUN(param, body)
+| v -> ERROR (sprintf "Got the follopwing term %A" v)
+
+// Here are two convenient abbreviations for using your interpreter.
+let interpfile filename = filename |> parsefile |> interp
+
+let interpstr sourcecode = sourcecode |> parsestr |> interp
